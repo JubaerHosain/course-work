d6545c78671ec2195f7a06359fca072a7ccd467a
jubaer <jubaerhosain1119@gmail.com>
2022-04-18 21:07:12 +0600

    Complated CYK Algorithm

--- Theory of Computing/CYK Algorithm/CYK_Aalgorithm.cpp
+++ Theory of Computing/CYK Algorithm/CYK_Aalgorithm.cpp
@@ -9,7 +9,7 @@ typedef vector<vector<int>> vii;
 const int N = 100;
 string table[N][N];
 // {child, {parents of it}}
-map<string, vector<string>> rules;
+map<string, vector<char>> rules;
 
 string remove_spaces(string &word) {
     string new_word;
@@ -17,6 +17,16 @@ string remove_spaces(string &word) {
         if(ch != ' ')
             new_word += ch;
     }
+    return new_word;
+}
+
+string remove_duplicates(string &word) {
+    set<char> unique;
+    for(char ch: word)
+        unique.insert(ch);
+    string new_word = "";
+    for(char ch: unique)
+        new_word += ch;
     return new_word;
 }
 
@@ -53,13 +63,78 @@ vector<string> parser(string &input) {
     return words;
 }
 
-void cyk_algorithm() {
-    char start;
+string get_symbols(int i, int j) {
+    string symbols;
+    for(int x = i; x < j; x++) {
+        string s1 = table[i][x];
+        string s2 = table[x+1][j];
+        if(s1.length() == 0 || s2.length() == 0) continue;
+        for(int a = 0; a < s1.length(); a++) {
+            for(int b = 0; b < s2.length(); b++) {
+                string word;
+                word.push_back(s1[a]);
+                word.push_back(s2[b]);
+                if(!rules.count(word)) continue;
+                vector<char> parents = rules[word];
+                for(char ch: parents)
+                    symbols.push_back(ch);
+            }
+        }
+    } 
+    symbols = remove_duplicates(symbols);
+    return symbols;
+}
+
+void cyk_algorithm(string &input, char start_symbol) {
+    int n = input.size();
+
+    // for all one length
+    for(int i = 0; i < n; i++) {
+        string parents;
+        string word;
+        word.push_back(input[i]);
+        if(rules.count(word)) {
+            vector<char> list = rules[word];
+            for(char ch: list) 
+                parents.push_back(ch);
+        }
+        table[i][i] = parents;
+    }
+
+    // from length 2 to upper
+    for(int l = 2; l <= n; l++) {
+        for(int i = 0; i <= n-l; i++) {
+            int j = i + l - 1;
+            table[i][j] = get_symbols(i, j);
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) 
+            cout << table[i][j] << " ";
+        cout << endl;
+    }
+
+    // column 1, n
+    string last = table[0][n-1];
+    for(char ch: last) {
+        if(ch == start_symbol) {
+            cout << "String is accepted by this language!" << endl;
+            return;
+        }
+    }
+    cout << "String is not accepted by this language!" << endl;
+}
+
+/*****main function*****/
+int main() {
+
+    char start_symbol;
     int no_of_rule;
     string input;
 
     cout << "Enter starting symbol: ";
-    cin >> start;
+    cin >> start_symbol;
 
     cout << "Enter number of productin rule: ";
     cin >> no_of_rule;
@@ -70,17 +145,22 @@ void cyk_algorithm() {
         cin >> rule;
         vector<string> words = parser(rule);
         for(int i = 1; i < words.size(); i++)
-            rules[words[i]].push_back(words[0]);
+            rules[words[i]].push_back(words[0][0]);
     }
+
+    // for(auto it: rules) {
+    //     cout << it.first << ": ";
+    //     for(auto it1: it.second)
+    //         cout << it1 << " ";
+    //     cout << endl;
+    // }
 
     cout << "Enter input string: ";
     cin >> input;
-}
 
-/*****main function*****/
-int main() {
+    cyk_algorithm(input, start_symbol);
 
-    cyk_algorithm();
+    
 
     return 0;
 } 
