74c2296e9a1957d2c8279b0d0e910ce0e738c22c
jubaer <jubaerhosain1119@gmail.com>
2022-05-12 23:44:52 +0600

    Updated

--- Numerical Analysis for Engineers(MATH304)/Numerical Integration/Simpson Rule.c
+++ Numerical Analysis for Engineers(MATH304)/Numerical Integration/Simpson Rule.c
@@ -2,8 +2,6 @@
 #include<math.h>
 #include<stdlib.h>
 
-// trapezoid rule
-
 double f(double x) {
     return (x*x) + 1;
 }
@@ -14,18 +12,21 @@ int main() {
     double a = 0, b = 1;
     double h = (b-a) / n;
 
-
     double x = 0;
-    double sum = 0;
-    sum = (f(a) + f(b)) / 2;
+    double area = f(a) + f(b);
+    double odd = 0, even = 0;
     for(int i = 1; i < n; i++) {
         x += h;
-        sum += f(x);
+        if(i%2) 
+            odd += f(x);
+        else
+            even += f(x);
     }
 
-    sum *= h;
+    area = area + 4 * odd + 2 * even;
+    area = area * (h/3);
 
-    printf("Area(Trapezoid): %.9lf\n", sum);
+    printf("Area(Simpson): %.9lf\n", area);
     
     return 0;
 }
--- Numerical Analysis for Engineers(MATH304)/Numerical Integration/Trapezoid Rule.c
+++ Numerical Analysis for Engineers(MATH304)/Numerical Integration/Trapezoid Rule.c
@@ -2,8 +2,6 @@
 #include<math.h>
 #include<stdlib.h>
 
-// simpson  rule
-
 double f(double x) {
     return (x*x) + 1;
 }
@@ -14,21 +12,18 @@ int main() {
     double a = 0, b = 1;
     double h = (b-a) / n;
 
+
     double x = 0;
-    double area = f(a) + f(b);
-    double odd = 0, even = 0;
+    double sum = 0;
+    sum = (f(a) + f(b)) / 2;
     for(int i = 1; i < n; i++) {
         x += h;
-        if(i%2) 
-            odd += f(x);
-        else
-            even += f(x);
+        sum += f(x);
     }
 
-    area = area + 4 * odd + 2 * even;
-    area = area * (h/3);
+    sum *= h;
 
-    printf("Area(Simpson): %.9lf\n", area);
+    printf("Area(Trapezoid): %.9lf\n", sum);
     
     return 0;
 }
