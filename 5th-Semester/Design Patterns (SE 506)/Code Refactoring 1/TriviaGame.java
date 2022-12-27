29ec6d9482a1f00ffed04cb0e29cdaec4d6aef17
JubaerHosain <jubaerhosain1119@gmail.com>
2022-12-27 12:08:54 +0600

    Updated

--- 5th-Semester/Design Patterns (SE 506)/Code Refactoring 1/TriviaGame.java
+++ 5th-Semester/Design Patterns (SE 506)/Code Refactoring 1/TriviaGame.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -21,72 +20,63 @@ public class TriviaGame {
             popQuestions.addLast("Pop Question " + i);
             scienceQuestions.addLast(("Science Question " + i));
             sportsQuestions.addLast(("Sports Question " + i));
-            rockQuestions.addLast(createRockQuestion(i));
+            rockQuestions.addLast("Rock Question " + i);	//createRockQuestion() method deleted (unnecessary method)
         }
     }
 
-    public String createRockQuestion(int index) {
-        return "Rock Question " + index;
-    }
-
-    public boolean isPlayable() {
-        return (howManyPlayers() >= 2);
-    }
+   //Dead code was here
 
     public boolean add(String playerName) {
-
-
         players.add(playerName);
-        places[howManyPlayers()] = 0;
-        purses[howManyPlayers()] = 0;
-        inPenaltyBox[howManyPlayers()] = false;
+        places[players.size()] = 0;		//unnecessary method used here
+        purses[players.size()] = 0;
+        inPenaltyBox[players.size()] = false;
 
         announce(playerName + " was added");
         announce("They are player number " + players.size());
         return true;
     }
 
-    public int howManyPlayers() {
-        return players.size();
-    }
 
-    public void roll(int roll) {
+    public void roll(int roll) {		//long method smell
         announce(players.get(currentPlayer) + " is the current player");
         announce("They have rolled a " + roll);
 
         if (inPenaltyBox[currentPlayer]) {
             if (roll % 2 != 0) {
-                isGettingOutOfPenaltyBox = true;
-
-                announce(players.get(currentPlayer) + " is getting out of the penalty box");
-                places[currentPlayer] = places[currentPlayer] + roll;
-                if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
-
-                announce(players.get(currentPlayer)
-                        + "'s new location is "
-                        + places[currentPlayer]);
-                announce("The category is " + currentCategory());
-                askQuestion();
-            } else {
-                announce(players.get(currentPlayer) + " is not getting out of the penalty box");
-                isGettingOutOfPenaltyBox = false;
+                anounceAndAssign(true);
+                newLocatuion(roll);
             }
-
-        } else {
-
-            places[currentPlayer] = places[currentPlayer] + roll;
-            if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
-
-            announce(players.get(currentPlayer)
-                    + "'s new location is "
-                    + places[currentPlayer]);
-            announce("The category is " + currentCategory());
-            askQuestion();
+            else {
+                anounceAndAssign(false);
+            }
+        }
+        else {
+            newLocatuion(roll);
         }
 
     }
 
+    public void anounceAndAssign(boolean value)
+    {
+        isGettingOutOfPenaltyBox = value;
+        announce(players.get(currentPlayer) + " is getting out of the penalty box");
+    }
+
+    public void newLocatuion(int roll)
+    {
+        places[currentPlayer] = places[currentPlayer] + roll;
+        if (places[currentPlayer] > 11) places[currentPlayer] = places[currentPlayer] - 12;
+
+        announce(players.get(currentPlayer)
+                + "'s new location is "
+                + places[currentPlayer]);
+        announce("The category is " + currentCategory());
+        askQuestion();
+    }
+
     private void askQuestion() {
+
         if (currentCategory() == "Pop")
             announce(popQuestions.removeFirst());
         if (currentCategory() == "Science")
@@ -97,7 +87,7 @@ public class TriviaGame {
             announce(rockQuestions.removeFirst());
     }
 
-
+    //Repeated code smell
     private String currentCategory() {
         if (places[currentPlayer] == 0) return "Pop";
         if (places[currentPlayer] == 4) return "Pop";
@@ -114,40 +104,36 @@ public class TriviaGame {
     public boolean wasCorrectlyAnswered() {
         if (inPenaltyBox[currentPlayer]) {
             if (isGettingOutOfPenaltyBox) {
-                announce("Answer was correct!!!!");
-                purses[currentPlayer]++;
-                announce(players.get(currentPlayer)
-                        + " now has "
-                        + purses[currentPlayer]
-                        + " Gold Coins.");
-
-                boolean winner = didPlayerWin();
-                currentPlayer++;
-                if (currentPlayer == players.size()) currentPlayer = 0;
-
-                return winner;
+                addPlayers();
             } else {
-                currentPlayer++;
-                if (currentPlayer == players.size()) currentPlayer = 0;
+                checkOverFlow();
                 return true;
             }
-
-
         } else {
-
-            announce("Answer was correct!!!!");
-            purses[currentPlayer]++;
-            announce(players.get(currentPlayer)
-                    + " now has "
-                    + purses[currentPlayer]
-                    + " Gold Coins.");
-
-            boolean winner = didPlayerWin();
-            currentPlayer++;
-            if (currentPlayer == players.size()) currentPlayer = 0;
-
-            return winner;
+            addPlayers();
         }
+        return false;
+    }
+
+    public boolean addPlayers()
+    {
+        announce("Answer was correct!!!!");
+        purses[currentPlayer]++;
+        announce(players.get(currentPlayer)
+                + " now has "
+                + purses[currentPlayer]
+                + " Gold Coins.");
+
+        boolean winner = didPlayerWin();
+        checkOverFlow();
+
+        return winner;
+    }
+
+    public void checkOverFlow()		//Duplicate code
+    {
+        currentPlayer++;
+        if (currentPlayer == players.size()) currentPlayer = 0;
     }
 
     public boolean wrongAnswer() {
@@ -155,8 +141,7 @@ public class TriviaGame {
         announce(players.get(currentPlayer) + " was sent to the penalty box");
         inPenaltyBox[currentPlayer] = true;
 
-        currentPlayer++;
-        if (currentPlayer == players.size()) currentPlayer = 0;
+        checkOverFlow();
         return true;
     }
 
@@ -166,9 +151,5 @@ public class TriviaGame {
 
     protected void announce(Object message) {
         System.out.println(message);
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
     }
 }
